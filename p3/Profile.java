ff8a3647a450a0243f9c994485e8bd1c54835a18
Zetong Qi <38269249+zetongqi@users.noreply.github.com>
2018-10-29 14:16:58 -0500

    Add files via upload

--- p3/Profile.java
+++ p3/Profile.java
@@ -22,26 +22,29 @@ public class Profile<K extends Comparable<K>, V> {
 	
 	//initialize the hashtable and treemap
 	public Profile() {
-		this.hashtable = new HashTable<K, V>(10099, 0.7);
+		this.hashtable = new HashTable<K, V>(5071, 0.75);
 		this.treemap = new TreeMap<K, V>();
 	}
 	
-	//insert a key value pair in both the hashtable and treemap
+	//insert a key value pair in the hashtable and treemap
 	public void insert(K key, V value) {
 		this.hashtable.put(key, value);
 		this.treemap.put(key, value);
 	}
 	
+	//retrieve an element from hashtable and treemap
 	public void retrieve(K key) {
 		this.hashtable.get(key);
 		this.treemap.get(key);
 	}
 	
+	//insert and retrieve from hashtable and treemap for numElements times
 	public static void main(String[] args) {
 		if (args.length < 1) {
 			System.out.println("Expected 1 argument: <num_elements>");
 			System.exit(1);
 		}
+
 		int numElements = Integer.parseInt(args[0]);
 		
 		Profile<Integer, Integer> profile = new Profile<Integer, Integer>();
